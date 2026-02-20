92f6d455bdd16fbc7a12e6e69c38293d828ae938
Lavanic <oliver@theohrts.com>
2026-02-20 13:08:48 -0800

    Remove Dummy Lol

--- src/main.py
+++ src/main.py
@@ -34,9 +34,9 @@ def main():
     threads = [
         threading.Thread(target=ble_scanner.main, args=(shutdown_event, detective_holder), name="ble_scanner"),
         threading.Thread(target=bt_service.main, args=(shutdown_event,), name="bt_service"),
-        threading.Thread(target=dummy_tof_detector.main, args=(detective_holder, shutdown_event, args), name="tof_detector"),
+        # threading.Thread(target=dummy_tof_detector.main, args=(detective_holder, shutdown_event, args), name="tof_detector"),
         # threading.Thread(target=github_sync.main, args=(shutdown_event,), name="github_sync"),
-        # threading.Thread(target=tof_detector.main, args=(detective_holder, shutdown_event, args), name="tof_detector"),
+        threading.Thread(target=tof_detector.main, args=(detective_holder, shutdown_event, args), name="tof_detector"),
     ]
 
     for t in threads:
